9a6bcfdbe42133128c90a36891cdc821c3d78d7d
Youssef Atef <111428670+Youssef-Atef-20@users.noreply.github.com>
2026-01-28 18:53:17 +0200

    Remove unused user1 variable from Dashboard component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState("info");
   const navigate = useNavigate();
-  const user1 = JSON.parse(localStorage.getItem("users"));
+
   
 
   const logout = () => {
